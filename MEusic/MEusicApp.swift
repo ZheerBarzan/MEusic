5c947c9f95a8c1261c6f28972c1488a4f4aeb712
zheer barzan <zheerbarzan85@outlook.com>
2024-11-13 19:03:59 +0300

    Initial Commit

---
+++ MEusic/MEusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MEusicApp.swift
+//  MEusic
+//
+//  Created by Zheer Barzan on 13/11/24.
+//
+
+import SwiftUI
+
+@main
+struct MEusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
